33da8b98a69512e3dec81d65c6d53d0503f7195c
Albert Yin <albyin@gmail.com>
2015-07-13 10:19:25 -0400

    refactor API, add env, posts and comments api

--- public/javascripts/app/controllers/main.js
+++ public/javascripts/app/controllers/main.js
@@ -1,4 +1,17 @@
-angular.module("ngMyApp", [])
-	.controller("mainController", ["$scope", function($scope){
+var app = angular.module("ngMyApp", []);
+
+app.controller("mainController", ["$scope", "Posts", "Comments", function($scope, Posts, Comments){
 		$scope.title = "This is a controller";
+
+		Posts.get()
+			.then(function (data){
+				$scope.posts = data;
+			});
+
+		Comments.get()
+			.then(function (data){
+				$scope.comments = data;
+			});
+
+
 	}]);
---
+++ public/javascripts/app/factories/MyAppAPI.js
@@ -0,0 +1,44 @@
+app.factory("APIConfig", [function (){
+	return {
+		devRoot : "http://jsonplaceholder.typicode.com/",
+		prodRoot: "",
+		env : "dev",
+		setEnv : function (){
+			
+		},
+		getRoot : function () {
+			//if env is dev, return devRoot, else return prodRoot
+			if (this.env === "dev"){
+				return this.devRoot;
+			}
+			else if (this.env ==="prod"){
+				return this.prodRoot;
+			}
+
+			return null;
+		}
+	};
+}]);
+
+app.factory("Posts", ["$http", "APIConfig", function($http, APIConfig){
+  return {
+    get : function() {
+      return $http.get(APIConfig.getRoot() + "posts/")
+      	.then(function(res){
+      		return res.data;
+      	});
+    }
+  };
+}]);
+
+app.factory("Comments", ["$http", "APIConfig", function($http, APIConfig){
+	return {
+		get : function (){
+			return $http.get(APIConfig.getRoot() + "comments/")
+      	.then(function(res){
+      		console.log("comments response", res);
+      		return res.data;
+      	});
+		}
+	};
+}]);
