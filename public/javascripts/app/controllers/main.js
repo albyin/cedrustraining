0328ebec83027fb240f5e55607348668718e5155
Albert Yin <albyin@gmail.com>
2015-07-20 16:55:52 -0400

    remove duplicate code

--- public/javascripts/app/controllers/main.js
+++ public/javascripts/app/controllers/main.js
@@ -1,33 +1,3 @@
-var app = angular.module('ngMyApp', ['ngRoute']);
-
-app.config(['$routeProvider', '$locationProvider',
-	function($routeProvider, $locationProvider) {
-
-		//this locationProvider setting removes need for '#/' in URLs
-		$locationProvider.html5Mode({
-		  enabled: true,
-		  requireBase: false
-		});
-
-		//configure routes
-    $routeProvider.
-    	when('/', {
-        templateUrl: 'page/home',
-        controller: 'mainController'
-      }).
-      when('/posts', {
-        templateUrl: 'page/posts',
-        controller: 'postController'
-      }).
-      when('/members', {
-        templateUrl: 'page/members',
-        controller: 'userController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
-
 app.controller('mainController', ['$scope', 
 	function($scope){
 		//define title
